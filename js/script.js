d77736f2be44fcd8050edf0b5b15be1d251add2d
antizyk <sashamatvienko0@gmail.com>
2020-12-02 11:22:30 +0100

    Создал переменные переключателей, ролителя переключателей для дилигирования событий, а так же контента

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabsContent = document.querySelectorAll('.tabcontent'),
+		tabsParent = document.querySelector('.tabheader__items');
+
+
+
+});
