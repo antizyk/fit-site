ae5bbc4b927d7fd5a8f0a47e71d533930cf87c04
antizyk <sashamatvienko0@gmail.com>
2020-12-07 17:58:46 +0100

    Начал работу с таймером, создал функцию которая высчитывает количество дней, часов, минут и секунд до дедлайна

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+	//Tabs
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		tabsContent = document.querySelectorAll('.tabcontent'),
 		parentTabs = document.querySelector('.tabheader__items');
@@ -35,4 +36,22 @@ document.addEventListener('DOMContentLoaded', () => {
 			});
 		}
 	});
+
+	//Timer
+	const deadline = '2021-01-01';
+
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor(t / (1000 * 60 * 60 * 24)),
+			hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+			minuts = Math.floor((t / 1000 / 60) % 60),
+			seconds = Math.floor((t / 1000) % 60);
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minuts': minuts,
+			'seconds': seconds
+		};
+	}
 });
